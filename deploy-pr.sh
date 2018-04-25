1e3caae1285085f62516b66b779158f161cee7ad
Henri Beck <henribeck.dev@gmail.com>
2018-04-25 19:05:05 +0200

    Updated the alias of the pr deploy

--- deploy-pr.sh
+++ deploy-pr.sh
@@ -2,6 +2,8 @@
 
 set -eu;
 
+echo $CIRCLE_BRANCH;
+
 yarn run now deploy -e NODE_ENV=production --public --regions bru;
-yarn run now alias "pr-$CIRCLE_PR_NUMBER-henribeck";
+yarn run now alias "$CIRCLE_BRANCH-henribeck";
 yarn run now remove henribeck.im --yes --safe;
