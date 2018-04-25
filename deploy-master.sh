66afb464b633fd9048628063b7b91b0fda0890b6
Henri Beck <henribeck.dev@gmail.com>
2018-04-25 18:23:57 +0200

    Added setting the token Simplified the deploy workflow

--- deploy-master.sh
+++ deploy-master.sh
@@ -2,6 +2,6 @@
 
 set -eu;
 
-yarn run now deploy -t $NOW_TOKEN -e NODE_ENV=production --public;
-yarn run now alias -t $NOW_TOKEN;
-yarn run now remove henribeck.im --yes --safe -t $NOW_TOKEN;
+yarn run now deploy -e NODE_ENV=production --public;
+yarn run now alias;
+yarn run now remove henribeck.im --yes --safe;
--- deploy-pr.sh
+++ deploy-pr.sh
@@ -2,6 +2,6 @@
 
 set -eu;
 
-yarn run now deploy -t $NOW_TOKEN -e NODE_ENV=production --public;
-yarn run now alias "pr-$CIRCLE_PR_NUMBER-henribeck" -t $NOW_TOKEN;
-yarn run now remove henribeck.im --yes --safe -t $NOW_TOKEN;
+yarn run now deploy -e NODE_ENV=production --public;
+yarn run now alias "pr-$CIRCLE_PR_NUMBER-henribeck";
+yarn run now remove henribeck.im --yes --safe;
