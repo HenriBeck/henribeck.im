f63359b4fcb80bb81a8d1cba4cab07d8f45f102e
Henri Beck <henribeck.dev@gmail.com>
2018-04-25 19:13:07 +0200

    Removed auto deploying of PRs becuase I only have a limited amount of instances available

--- deploy-pr.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu;
-
-echo $CIRCLE_BRANCH;
-
-yarn run now deploy -e NODE_ENV=production --public --regions bru;
-yarn run now alias "$CIRCLE_BRANCH-henribeck";
-yarn run now remove henribeck.im --yes --safe;
