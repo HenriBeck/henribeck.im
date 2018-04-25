8a4e5797d24281a1e9bf7a0cb08d38b4b0d84420
Henri Beck <henribeck.dev@gmail.com>
2018-04-25 18:27:15 +0200

    Added deploying to the Brussels region

--- deploy-pr.sh
+++ deploy-pr.sh
@@ -2,6 +2,6 @@
 
 set -eu;
 
-yarn run now deploy -e NODE_ENV=production --public;
+yarn run now deploy -e NODE_ENV=production --public --regions bru;
 yarn run now alias "pr-$CIRCLE_PR_NUMBER-henribeck";
 yarn run now remove henribeck.im --yes --safe;
