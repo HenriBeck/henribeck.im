79dda00ce8f2a22885f9f89b70b111a851189b67
Henri Beck <henribeck.dev@gmail.com>
2018-06-29 22:08:42 +0200

    Fixed building
    Signed-off-by: Henri Beck <henribeck.dev@gmail.com>

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('bundle-analyzer-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const config = require('./webpack.config');
 
