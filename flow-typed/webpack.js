efd5b60d497eff996d71ba52f17846b3ea1d108d
Henri Beck <henribeck.dev@gmail.com>
2018-08-05 14:29:25 +0200

    Updated flow types

--- flow-typed/webpack.js
+++ flow-typed/webpack.js
@@ -4,59 +4,38 @@ declare module 'webpack-merge' {
   declare module.exports: (config1: {}, config2: {}) => {};
 }
 
-declare module 'html-webpack-plugin' {
-  declare class HTMLWebpackPlugin {
-    constructor(options: {}): this,
-  }
+declare class Plugin {
+  constructor(options: {}): this,
+}
 
-  declare module.exports: Class<HTMLWebpackPlugin>;
+declare module 'html-webpack-plugin' {
+  declare module.exports: Class<Plugin>;
 }
 
 declare module 'webpack-pwa-manifest' {
-  declare class WebpackPWAManifest {
-    constructor(options: {}): this,
-  }
-
-  declare module.exports: Class<WebpackPWAManifest>;
+  declare module.exports: Class<Plugin>;
 }
 
 declare module 'mini-css-extract-plugin' {
-  declare class MiniCSSExtractPlugin {
+  declare class MiniCSSExtractPlugin extends Plugin {
     static loader: {},
-    constructor(options: {}): this,
   }
 
   declare module.exports: Class<MiniCSSExtractPlugin>;
 }
 
 declare module 'uglifyjs-webpack-plugin' {
-  declare class UglifyJSWebpackPlugin {
-    constructor(options: {}): this,
-  }
-
-  declare module.exports: Class<UglifyJSWebpackPlugin>;
+  declare module.exports: Class<Plugin>;
 }
 
 declare module 'optimize-css-assets-webpack-plugin' {
-  declare class OptimizeCSSAssetsWebpackPlugin {
-    constructor(options: {}): this,
-  }
-
-  declare module.exports: Class<OptimizeCSSAssetsWebpackPlugin>;
+  declare module.exports: Class<Plugin>;
 }
 
 declare module 'sw-precache-webpack-plugin' {
-  declare class SWPrecacheWebpackPlugin {
-    constructor(options: {}): this,
-  }
-
-  declare module.exports: Class<SWPrecacheWebpackPlugin>;
+  declare module.exports: Class<Plugin>;
 }
 
 declare module 'webpack-bundle-analyzer' {
-  declare class BundleAnalyzerPlugin {
-    constructor(options: {}): this,
-  }
-
-  declare module.exports: { BundleAnalyzerPlugin: Class<BundleAnalyzerPlugin> };
+  declare module.exports: { BundleAnalyzerPlugin: Class<Plugin> };
 }
